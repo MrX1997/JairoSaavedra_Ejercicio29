39426c53c6b7d707a528bad5e364655c82d2c5c7
MrX1997 <ja.saavedra10@uniandes.edu.co>
2018-11-21 06:52:27 -0500

    Version Final

--- plons.py
+++ plons.py
@@ -4,9 +4,14 @@ import matplotlib.pylab as plt
 d = np.arange(0,5,1)
 #for i in range(5):
 for i in d:
-    dat = np.loadtxt("dat{:d}.txt".format(i))
-    x = dat[:,0]
-    u = dat[:,1]
-    plt.plot(x,u)
+	dat = np.loadtxt("dat{:d}.txt".format(i))
+	x = dat[:,0]
+	u = dat[:,1]
+	plt.plot(x,u,label='tiempo{:d}'.format(i))
+	plt.xlabel('X')
+	plt.xlabel('Y')
+	plt.title('Adveccion no lineal')
+	plt.legend()
 
-plt.savefig("Grafica.png")
+
+plt.savefig("Grafica.pdf")
